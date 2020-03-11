1fcdfd727068b8775175ccbf32a8c0df97d7b33e
Shaan <Shaan@admins-MacBook-Pro.local>
2020-03-10 19:26:46 -0700

    adding event handler

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,11 +6,18 @@ class Counter extends Component {
         tags: ['tag1', 'tag2', 'tag3']
     };
     
+
+
+    handleIncrement = () => {
+        console.log('Incremenet clicked', this);
+
+    };
+    
     render() { 
         return (
             <React.Fragment>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
                 <ul>
                     {this.state.tags.map(tag => <li key={tag}>{ tag }</li>)}
                 </ul>
