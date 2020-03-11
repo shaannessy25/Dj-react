eb797efdddf88f43d97f7c7092a13b02b1b4c203
Shaan <Shaan@admins-MacBook-Pro.local>
2020-03-10 23:32:19 -0700

    adding titles

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 class Counters extends Component {
-    state = {  }
+    state = {  
+        counters: [
+            { id: 1, value: 0},
+            { id: 2, value: 0},
+            { id: 3, value: 0},
+            { id: 4, value: 0},
+        ]
+    }
     render() { 
         return (  
             <div>
-                <Counter />
-                <Counter />
-                <Counter />
-                <Counter />
-                <Counter />
-                <Counter />
+                {this.state.counters.map(counter => 
+                <Counter key={counter.id} value={counter.value} id={counter.id}>
+                    <h4>This is counter #{counter.id}</h4>    
+                </Counter>)}
             </div>
         );
     }
 }
  
 export default Counters;
+ 
