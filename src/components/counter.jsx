ae04a9be2f54796bb20a91d41d15c878fe39e962
Shaan <Shaan@admins-MacBook-Pro.local>
2020-03-10 17:59:29 -0700

    displays h1 tag

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,4 +6,4 @@ class Counter extends Component {
     }
 }
  
-export default Counter;≈
+export default Counter; 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./index.css";
 import 'bootstrap/dist/css/bootstrap.css';
 import Counter from './components/counter';
 
 
 ReactDOM.render(<Counter />, document.getElementById('root'));
-console.log(element);
