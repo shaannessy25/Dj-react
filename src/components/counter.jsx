0717680ff2dcb1f7f9374ce37de380a92e65f7af
Shaan <Shaan@admins-MacBook-Pro.local>
2020-03-10 18:34:14 -0700

    adding button styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,12 +5,17 @@ class Counter extends Component {
         count: 0,
         imageUrl: 'https://picsum.photos/200'
     };
+
+    styles = {
+        fontSize: 10,
+        fontWeight: 'bold'
+    }
     render() { 
         return (
             <React.Fragment>
                 <img src={this.state.imageUrl} alt="not working"/>
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
     }
