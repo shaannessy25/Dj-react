04884a2f4c2c5c5fb11a91e1d20a1ad7168d971f
Shaan <Shaan@admins-MacBook-Pro.local>
2020-03-10 19:01:30 -0700

    formated counter badge

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,14 +6,17 @@ class Counter extends Component {
     };
     
     render() { 
-        let classes = 'badge m-2 badge-';
-        classes += (this.state.count === 0) ? "warning" : "primary";
         return (
             <React.Fragment>
-                <span style={this.styles} className={classes}>{this.formatCount()}</span>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
+    }
+    getBadgeClasses() {
+        let classes = 'badge m-2 badge -';
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
