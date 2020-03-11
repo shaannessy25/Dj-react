1aa941d07e61ca20d26e84dd515163b624a24659
Shaan <Shaan@admins-MacBook-Pro.local>
2020-03-10 18:48:22 -0700

    badge changes color based on count

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,19 +2,15 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        imageUrl: 'https://picsum.photos/200'
+        count: 1,
     };
-
-    styles = {
-        fontSize: 10,
-        fontWeight: 'bold'
-    }
+    
     render() { 
+        let classes = 'badge m-2 badge-';
+        classes += (this.state.count === 0) ? "warning" : "primary";
         return (
             <React.Fragment>
-                <img src={this.state.imageUrl} alt="not working"/>
-                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span style={this.styles} className={classes}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
