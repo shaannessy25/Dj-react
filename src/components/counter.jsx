63899b146baf525825e52229f2d57681cc1aa840
Shaan <Shaan@admins-MacBook-Pro.local>
2020-03-10 23:12:58 -0700

    adding multiple increment buttons

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,21 +3,20 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        tags: ['tag1', 'tag2', 'tag3']
     };
     
 
 
     handleIncrement = () => {
         this.setState({ count: this.state.count + 1 })
-        console.log('hanlde is working')
+        
     };
 
     render() { 
         return (
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.handleIncrement()} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
